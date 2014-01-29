dbf839ef225bd5ea0b3a884fb5b0b2905b6d6747
Matias Bjørling <m@bjorling.me>
2014-01-28 16:41:41 -0800

    specu: current version
    global atomic "global_iowait"

--- arch/x86/include/asm/current.h
+++ arch/x86/include/asm/current.h
@@ -9,13 +9,21 @@ struct task_struct;
 
 DECLARE_PER_CPU(struct task_struct *, current_task);
 
+static atomic_t __global_iowait;
+
 static __always_inline struct task_struct *get_current(void)
 {
 	return this_cpu_read_stable(current_task);
 }
 
+static __always_inline atomic_t *get_global_iowait(void)
+{
+	return &__global_iowait;
+}
+
 #define current get_current()
 
+#define global_iowait get_global_iowait()
 #endif /* __ASSEMBLY__ */
 
 #endif /* _ASM_X86_CURRENT_H */
